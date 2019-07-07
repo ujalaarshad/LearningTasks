d81897e097a033e7de80e27f234ef20aa628fe7a
ujalaarshad <38494011+ujalaarshad@users.noreply.github.com>
2019-07-07 13:43:35 +0500

    login with google in asp .net core

---
+++ Login_with_Google/Login_with_Google/Controllers/GoogleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login_with_Google.Controllers
+{
+    public class GoogleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
